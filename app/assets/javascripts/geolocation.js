d430fed871df47fb138b7a34afb4c9cad48b51fc
rustyhutto <rhutto92@gmail.com>
2016-10-16 17:40:52 -0700

    Add google geocoder on click

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -1,3 +1,25 @@
-function getCityState() {
-  return "city, state"
+function initMap() {
+    // making new geocoder
+    var geocoder = new google.maps.Geocoder;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            // geocodeLatLng(geocoder, pos);
+            console.log(geocoder);
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 }
+
+$(function() {
+    $("#launch").on("click", function() {
+        initMap();
+    })
+});
