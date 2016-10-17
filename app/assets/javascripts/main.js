37342df90e0f07993d5d2886b13b76055f059842
rustyhutto <rhutto92@gmail.com>
2016-10-16 17:40:25 -0700

    Comment out function

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,3 @@
-$(document).ready(function() {
-  console.log(textToSpeech(getWikipediaOverview(getCityState())))
-});
+// $(document).ready(function() {
+//   console.log(textToSpeech(getWikipediaOverview(getCityState())))
+// });
