cdc4c1ea22acef747a616c9fad2a9556fc742b1b
rustyhutto <rhutto92@gmail.com>
2016-10-16 19:23:54 -0700

    Query wikipedia for overview

--- app/assets/javascripts/wikipediaOverview.js
+++ app/assets/javascripts/wikipediaOverview.js
@@ -1,3 +1,12 @@
-function getWikipediaOverview(cityState) {
-  return "wiki overview for " + cityState
+function queryWikipedia(query) {
+    console.log("querying wikipedia for: " + query)
+    var re = / /g
+    var formattedQuery = query.replace(re, "_")
+    var queryUrl = "https://query-wikipedia.herokuapp.com/?params=" + formattedQuery
+    $.ajax({
+        url: queryUrl
+    }).done(function(response) {
+        console.log("sending to TTS")
+        $(".container").html(response)
+    });
 }
