874e4cdc448e59907b3d8b1d639c3fb2bffe30ee
rustyhutto <rhutto92@gmail.com>
2016-10-18 11:08:44 -0700

    Remove route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :search do
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
 end
