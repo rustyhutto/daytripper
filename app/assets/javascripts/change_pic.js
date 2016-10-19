95ab06e927b8e2d5f23e736fa844fb5c34ce491a
Jessica Forchione <jsforchione@gmail.com>
2016-10-19 11:01:33 -0700

    Allow pics to be changed with search or geolocation, change if to switch statement

--- app/assets/javascripts/change_pic.js
+++ app/assets/javascripts/change_pic.js
@@ -1,103 +1,254 @@
-function setPic() {
-    if (state === "AL") {
-        $('body').css('background', "url(assets/alabama.png)");
-    } else if (state === "AK") {
-        $('body').css('background', "url(assets/alaska.png)");
-    } else if (state === "AZ") {
-        $('body').css('background', "url(assets/arizona.png)");
-    } else if (state === "AR") {
-        $('body').css('background', "url(assets/arkansas.png)");
-    } else if (state === "CA") {
-        $('body').css('background', "url(assets/california.png)");
-    } else if (state === "CO") {
-        $('body').css('background', "url(assets/colorado.png)");
-    } else if (state === "CT") {
-        $('body').css('background', "url(assets/connecticut.png)");
-    } else if (state === "DE") {
-        $('body').css('background', "url(assets/delaware.png)");
-    } else if (state === "FL") {
-        $('body').css('background', "url(assets/florida.png)");
-    } else if (state === "GA") {
-        $('body').css('background', "url(assets/georgia.png)");
-    } else if (state === "HI") {
-        $('body').css('background', "url(assets/hawaii.png)");
-    } else if (state === "ID") {
-        $('body').css('background', "url(assets/idaho.png)");
-    } else if (state === "IL") {
-        $('body').css('background', "url(assets/illinois.png)");
-    } else if (state === "IN") {
-        $('body').css('background', "url(assets/indiana.png)");
-    } else if (state === "IA") {
-        $('body').css('background', "url(assets/iowa.png)");
-    } else if (state === "KS") {
-        $('body').css('background', "url(assets/kansas.png)");
-    } else if (state === "KY") {
-        $('body').css('background', "url(assets/kentucky.png)");
-    } else if (state === "LA") {
-        $('body').css('background', "url(assets/louisiana.png)");
-    } else if (state === "ME") {
-        $('body').css('background', "url(assets/maryland.png)");
-    } else if (state === "MD") {
-        $('body').css('background', "url(assets/maine.png)");
-    } else if (state === "MA") {
-        $('body').css('background', "url(assets/massachusetts.png)");
-    } else if (state === "MI") {
-        $('body').css('background', "url(assets/michigan.png)");
-    } else if (state === "MN") {
-        $('body').css('background', "url(assets/minnesota.png)");
-    } else if (state === "MS") {
-        $('body').css('background', "url(assets/mississippi.png)");
-    } else if (state === "MO") {
-        $('body').css('background', "url(assets/missouri.png)");
-    } else if (state === "MT") {
-        $('body').css('background', "url(assets/montana.png)");
-    } else if (state === "NE") {
-        $('body').css('background', "url(assets/nebraska.png)");
-    } else if (state === "NV") {
-        $('body').css('background', "url(assets/nevada.png)");
-    } else if (state === "NH") {
-        $('body').css('background', "url(assets/new_hampshire.png)");
-    } else if (state === "NJ") {
-        $('body').css('background', "url(assets/new_jersey.png)");
-    }  else if (state === "NM") {
-        $('body').css('background', "url(assets/new_mexico.png)");
-    }  else if (state === "NY") {
-        $('body').css('background', "url(assets/new_york.png)");
-    }  else if (state === "NC") {
-        $('body').css('background', "url(assets/north_carolina.png)");
-    }  else if (state === "ND") {
-        $('body').css('background', "url(assets/north_dakota.png)");
-    }  else if (state === "OH") {
-        $('body').css('background', "url(assets/ohio.png)");
-    }  else if (state === "OK") {
-        $('body').css('background', "url(assets/oklahoma.png)");
-    }  else if (state === "OR") {
-        $('body').css('background', "url(assets/oregon.png)");
-    }  else if (state === "PA") {
-        $('body').css('background', "url(assets/pennsylvania.png)");
-    }  else if (state === "RI") {
-        $('body').css('background', "url(assets/rhode_island.png)");
-    }  else if (state === "SC") {
-        $('body').css('background', "url(assets/south_carolina.png)");
-    }  else if (state === "SD") {
-        $('body').css('background', "url(assets/south_dakota.png)");
-    }  else if (state === "TN") {
-        $('body').css('background', "url(assets/tennessee.png)");
-    }  else if (state === "TX") {
-        $('body').css('background', "url(assets/texas.png)");
-    }  else if (state === "UT") {
-        $('body').css('background', "url(assets/utah.png)");
-    }  else if (state === "VT") {
-        $('body').css('background', "url(assets/vermont.png)");
-    }  else if (state === "VA") {
-        $('body').css('background', "url(assets/virginia.png)");
-    }  else if (state === "WA") {
-        $('body').css('background', "url(assets/washington.png)");
-    }  else if (state === "WV") {
-        $('body').css('background', "url(assets/west_virginia.png)");
-    }  else if (state === "WI") {
-        $('body').css('background', "url(assets/wisconsin.png)");
-    }  else {
-        $('body').css('background', "url(assets/wyoming.png)");
+function setPic(state) {
+    switch (state) {
+        case "al" || "alabama":
+            $('body').css('background', "url(assets/alabama.png)");
+            break;
+        case "ak" || "arkansas":
+            $('body').css('background', "url(assets/arkansas.png)");
+            break;
+        case "az" || "arizona":
+            $('body').css('background', "url(assets/arizona.png)");
+            break;
+        case "ca" || "california":
+            $('body').css('background', "url(assets/california.png)");
+            break;
+        case "co" || "colorado":
+            $('body').css('background', "url(assets/colorado.png)");
+            break;
+        case "ct" || "connecticut":
+            $('body').css('background', "url(assets/connecticut.png)");
+            break;
+        case "de" || "delaware":
+            $('body').css('background', "url(assets/delaware.png)");
+            break;
+        case "fl" || "florida":
+            $('body').css('background', "url(assets/florida.png)");
+            break;
+        case "ga" || "georgia":
+            $('body').css('background', "url(assets/georgia.png)");
+            break;
+        case "hi" || "hawaii":
+            $('body').css('background', "url(assets/hawaii.png)");
+            break;
+        case "id" || "idaho":
+            $('body').css('background', "url(assets/idaho.png)");
+            break;
+        case "il" || "illinois":
+            $('body').css('background', "url(assets/illinois.png)");
+            break;
+        case "in" || "indiana":
+            $('body').css('background', "url(assets/indiana.png)");
+            break;
+        case "ia" || "iowa":
+            $('body').css('background', "url(assets/iowa.png)");
+            break;
+        case "ks" || "kansas":
+            $('body').css('background', "url(assets/kansas.png)");
+            break;
+        case "ky" || "kentucky":
+            $('body').css('background', "url(assets/kentucky.png)");
+            break;
+        case "la" || "louisiana":
+            $('body').css('background', "url(assets/louisiana.png)");
+            break;
+        case "md" || "maryland":
+            $('body').css('background', "url(assets/maryland.png)");
+            break;
+        case "me" || "maine":
+            $('body').css('background', "url(assets/maine.png)");
+            break;
+        case "ma" || "massachusetts":
+            $('body').css('background', "url(assets/massachusetts.png)");
+            break;
+        case "mi" || "michigan":
+            $('body').css('background', "url(assets/michigan.png)");
+            break;
+        case "mn" || "minnesota":
+            $('body').css('background', "url(assets/minnesota.png)");
+            break;
+        case "ms" || "mississippi":
+            $('body').css('background', "url(assets/mississippi.png)");
+            break;
+        case "mo" || "missouri":
+            $('body').css('background', "url(assets/missouri.png)");
+            break;
+        case "mt" || "montana":
+            $('body').css('background', "url(assets/montana.png)");
+            break;
+        case "ne" || "nebraska":
+            $('body').css('background', "url(assets/nebraska.png)");
+            break;
+        case "nv" || "nevada":
+            $('body').css('background', "url(assets/nevada.png)");
+            break;
+        case "nh" || "new hampshire":
+            $('body').css('background', "url(assets/new_hampshire.png)");
+            break;
+        case "nj" || "new jersey":
+            $('body').css('background', "url(assets/new_jersey.png)");
+            break;
+        case "nm" || "new mexico":
+            $('body').css('background', "url(assets/new_mexico.png)");
+            break;
+        case "ny" || "new york":
+            $('body').css('background', "url(assets/new_york.png)");
+            break;
+        case "nc" || "north carolina":
+            $('body').css('background', "url(assets/north_carolina.png)");
+            break;
+        case "nd" || "north dakota":
+            $('body').css('background', "url(assets/north_dakota.png)");
+            break;
+        case "oh" || "ohio":
+            $('body').css('background', "url(assets/ohio.png)");
+            break;
+        case "ok" || "oklahoma":
+            $('body').css('background', "url(assets/oklahoma.png)");
+            break;
+        case "or" || "oregon":
+            $('body').css('background', "url(assets/oregon.png)");
+            break;
+        case "pa" || "pennsylvania":
+            $('body').css('background', "url(assets/pennsylvania.png)");
+            break;
+        case "ri" || "rhode island":
+            $('body').css('background', "url(assets/rhode_island.png)");
+            break;
+        case "sc" || "south carolina":
+            $('body').css('background', "url(assets/south_carolina.png)");
+            break;
+        case "sd" || "south dakota":
+            $('body').css('background', "url(assets/south_dakota.png)");
+            break;
+        case "tn" || "tennessee":
+            $('body').css('background', "url(assets/tennessee.png)");
+            break;
+        case "tx" || "texas":
+            $('body').css('background', "url(assets/texas.png)");
+            break;
+        case "ut" || "utah":
+            $('body').css('background', "url(assets/utah.png)");
+            break;
+        case "vt" || "vermont":
+            $('body').css('background', "url(assets/vermont.png)");
+            break;
+        case "va" || "virginia":
+            $('body').css('background', "url(assets/virginia.png)");
+            break;
+        case "wa" || "washington":
+            $('body').css('background', "url(assets/washington.png)");
+            break;
+        case "wv" || "west virginia":
+            $('body').css('background', "url(assets/west_virginia.png)");
+            break;
+        case "wi" || "wisconsin":
+            $('body').css('background', "url(assets/wisconsin.png)");
+            break;
+        case "wy" || "wyoming":
+            $('body').css('background', "url(assets/wyoming.png)");
+            break;
+        default:
+            $('body').css('background', "url(assets/default.png)");
     }
+    // if (state === "AL") {
+    //     $('body').css('background', "url(assets/alabama.png)");
+    // } else if (state === "AK") {
+    //     $('body').css('background', "url(assets/alaska.png)");
+    // } else if (state === "AZ") {
+    //     $('body').css('background', "url(assets/arizona.png)");
+    // } else if (state === "AR") {
+    //     $('body').css('background', "url(assets/arkansas.png)");
+    // } else if (state === "CA") {
+    //     $('body').css('background', "url(assets/california.png)");
+    // } else if (state === "CO") {
+    //     $('body').css('background', "url(assets/colorado.png)");
+    // } else if (state === "CT") {
+    //     $('body').css('background', "url(assets/connecticut.png)");
+    // } else if (state === "DE") {
+    //     $('body').css('background', "url(assets/delaware.png)");
+    // } else if (state === "FL") {
+    //     $('body').css('background', "url(assets/florida.png)");
+    // } else if (state === "GA") {
+    //     $('body').css('background', "url(assets/georgia.png)");
+    // } else if (state === "HI") {
+    //     $('body').css('background', "url(assets/hawaii.png)");
+    // } else if (state === "ID") {
+    //     $('body').css('background', "url(assets/idaho.png)");
+    // } else if (state === "IL") {
+    //     $('body').css('background', "url(assets/illinois.png)");
+    // } else if (state === "IN") {
+    //     $('body').css('background', "url(assets/indiana.png)");
+    // } else if (state === "IA") {
+    //     $('body').css('background', "url(assets/iowa.png)");
+    // } else if (state === "KS") {
+    //     $('body').css('background', "url(assets/kansas.png)");
+    // } else if (state === "KY") {
+    //     $('body').css('background', "url(assets/kentucky.png)");
+    // } else if (state === "LA") {
+    //     $('body').css('background', "url(assets/louisiana.png)");
+    // } else if (state === "ME") {
+    //     $('body').css('background', "url(assets/maryland.png)");
+    // } else if (state === "MD") {
+    //     $('body').css('background', "url(assets/maine.png)");
+    // } else if (state === "MA") {
+    //     $('body').css('background', "url(assets/massachusetts.png)");
+    // } else if (state === "MI") {
+    //     $('body').css('background', "url(assets/michigan.png)");
+    // } else if (state === "MN") {
+    //     $('body').css('background', "url(assets/minnesota.png)");
+    // } else if (state === "MS") {
+    //     $('body').css('background', "url(assets/mississippi.png)");
+    // } else if (state === "MO") {
+    //     $('body').css('background', "url(assets/missouri.png)");
+    // } else if (state === "MT") {
+    //     $('body').css('background', "url(assets/montana.png)");
+    // } else if (state === "NE") {
+    //     $('body').css('background', "url(assets/nebraska.png)");
+    // } else if (state === "NV") {
+    //     $('body').css('background', "url(assets/nevada.png)");
+    // } else if (state === "NH") {
+    //     $('body').css('background', "url(assets/new_hampshire.png)");
+    // } else if (state === "NJ") {
+    //     $('body').css('background', "url(assets/new_jersey.png)");
+    // }  else if (state === "NM") {
+    //     $('body').css('background', "url(assets/new_mexico.png)");
+    // }  else if (state === "NY") {
+    //     $('body').css('background', "url(assets/new_york.png)");
+    // }  else if (state === "NC") {
+    //     $('body').css('background', "url(assets/north_carolina.png)");
+    // }  else if (state === "ND") {
+    //     $('body').css('background', "url(assets/north_dakota.png)");
+    // }  else if (state === "OH") {
+    //     $('body').css('background', "url(assets/ohio.png)");
+    // }  else if (state === "OK") {
+    //     $('body').css('background', "url(assets/oklahoma.png)");
+    // }  else if (state === "OR") {
+    //     $('body').css('background', "url(assets/oregon.png)");
+    // }  else if (state === "PA") {
+    //     $('body').css('background', "url(assets/pennsylvania.png)");
+    // }  else if (state === "RI") {
+    //     $('body').css('background', "url(assets/rhode_island.png)");
+    // }  else if (state === "SC") {
+    //     $('body').css('background', "url(assets/south_carolina.png)");
+    // }  else if (state === "SD") {
+    //     $('body').css('background', "url(assets/south_dakota.png)");
+    // }  else if (state === "TN") {
+    //     $('body').css('background', "url(assets/tennessee.png)");
+    // }  else if (state === "TX") {
+    //     $('body').css('background', "url(assets/texas.png)");
+    // }  else if (state === "UT") {
+    //     $('body').css('background', "url(assets/utah.png)");
+    // }  else if (state === "VT") {
+    //     $('body').css('background', "url(assets/vermont.png)");
+    // }  else if (state === "VA") {
+    //     $('body').css('background', "url(assets/virginia.png)");
+    // }  else if (state === "WA") {
+    //     $('body').css('background', "url(assets/washington.png)");
+    // }  else if (state === "WV") {
+    //     $('body').css('background', "url(assets/west_virginia.png)");
+    // }  else if (state === "WI") {
+    //     $('body').css('background', "url(assets/wisconsin.png)");
+    // }  else {
+    //     $('body').css('background', "url(assets/wyoming.png)");
+    // }
 }
