86f6f79c0186a9df11413cc83578fb1c43b906da
rustyhutto <rhutto92@gmail.com>
2016-10-19 13:44:58 -0700

    Format color and text of loading circle

--- app/assets/javascripts/loading.js
+++ app/assets/javascripts/loading.js
@@ -1,17 +1,16 @@
 $(function() {
     window.onload = function onLoad() {
         var circle = new ProgressBar.Circle('#loading', {
-            color: 'green',
+            color: '#33A532',
             strokeWidth: 3,
             trailWidth: 1,
-            height: "30px",
             duration: 4000,
             easing: 'easeInOut',
             text: {
-                autoStyleContainer: false
+                // autoStyleContainer: false
             },
-            from: { color: '#aaa', width: 1 },
-            to: { color: '#333', width: 4 },
+            from: { color: '#F9152F', width: 1 },
+            to: { color: '#33A532', width: 4 },
             // Set default step function for all animate calls
             step: function(state, circle) {
                 circle.path.setAttribute('stroke', state.color);
