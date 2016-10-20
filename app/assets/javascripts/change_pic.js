d27fbb50633ef448a3acd462a6750f7c7cb52cbd
Jessica Forchione <jsforchione@gmail.com>
2016-10-19 17:00:39 -0700

    Add default pic

--- app/assets/javascripts/change_pic.js
+++ app/assets/javascripts/change_pic.js
@@ -4,150 +4,150 @@ function setPic(state) {
             $('body').css('background', "url(assets/alabama.jpg)");
             break;
         case "ak" || "arkansas":
-            $('body').css('background', "url(assets/arkansas.png)");
+            $('body').css('background', "url(assets/arkansas.jpeg)");
             break;
         case "az" || "arizona":
-            $('body').css('background', "url(assets/arizona.png)");
+            $('body').css('background', "url(assets/arizona.jpeg)");
             break;
         case "ca" || "california":
-            $('body').css('background', "url(assets/california.png)");
+            $('body').css('background', "url(assets/california.jpeg)");
             break;
         case "co" || "colorado":
-            $('body').css('background', "url(assets/colorado.png)");
+            $('body').css('background', "url(assets/colorado.jpeg)");
             break;
         case "ct" || "connecticut":
-            $('body').css('background', "url(assets/connecticut.png)");
+            $('body').css('background', "url(assets/connecticut.jpeg)");
             break;
         case "de" || "delaware":
-            $('body').css('background', "url(assets/delaware.png)");
+            $('body').css('background', "url(assets/delaware.jpeg)");
             break;
         case "fl" || "florida":
-            $('body').css('background', "url(assets/florida.png)");
+            $('body').css('background', "url(assets/florida.jpeg)");
             break;
         case "ga" || "georgia":
-            $('body').css('background', "url(assets/georgia.png)");
+            $('body').css('background', "url(assets/georgia.jpeg)");
             break;
         case "hi" || "hawaii":
-            $('body').css('background', "url(assets/hawaii.png)");
+            $('body').css('background', "url(assets/hawaii.jpeg)");
             break;
         case "id" || "idaho":
-            $('body').css('background', "url(assets/idaho.png)");
+            $('body').css('background', "url(assets/idaho.jpeg)");
             break;
         case "il" || "illinois":
-            $('body').css('background', "url(assets/illinois.png)");
+            $('body').css('background', "url(assets/illinois.jpeg)");
             break;
         case "in" || "indiana":
-            $('body').css('background', "url(assets/indiana.png)");
+            $('body').css('background', "url(assets/indiana.jpeg)");
             break;
         case "ia" || "iowa":
-            $('body').css('background', "url(assets/iowa.png)");
+            $('body').css('background', "url(assets/iowa.jpeg)");
             break;
         case "ks" || "kansas":
-            $('body').css('background', "url(assets/kansas.png)");
+            $('body').css('background', "url(assets/kansas.jpeg)");
             break;
         case "ky" || "kentucky":
-            $('body').css('background', "url(assets/kentucky.png)");
+            $('body').css('background', "url(assets/kentucky.jpeg)");
             break;
         case "la" || "louisiana":
-            $('body').css('background', "url(assets/louisiana.png)");
+            $('body').css('background', "url(assets/louisiana.jpeg)");
             break;
         case "md" || "maryland":
-            $('body').css('background', "url(assets/maryland.png)");
+            $('body').css('background', "url(assets/maryland.jpeg)");
             break;
         case "me" || "maine":
-            $('body').css('background', "url(assets/maine.png)");
+            $('body').css('background', "url(assets/maine.jpeg)");
             break;
         case "ma" || "massachusetts":
-            $('body').css('background', "url(assets/massachusetts.png)");
+            $('body').css('background', "url(assets/massachusetts.jpeg)");
             break;
         case "mi" || "michigan":
-            $('body').css('background', "url(assets/michigan.png)");
+            $('body').css('background', "url(assets/michigan.jpeg)");
             break;
         case "mn" || "minnesota":
-            $('body').css('background', "url(assets/minnesota.png)");
+            $('body').css('background', "url(assets/minnesota.jpeg)");
             break;
         case "ms" || "mississippi":
-            $('body').css('background', "url(assets/mississippi.png)");
+            $('body').css('background', "url(assets/mississippi.jpeg)");
             break;
         case "mo" || "missouri":
-            $('body').css('background', "url(assets/missouri.png)");
+            $('body').css('background', "url(assets/missouri.jpeg)");
             break;
         case "mt" || "montana":
-            $('body').css('background', "url(assets/montana.png)");
+            $('body').css('background', "url(assets/montana.jpeg)");
             break;
         case "ne" || "nebraska":
-            $('body').css('background', "url(assets/nebraska.png)");
+            $('body').css('background', "url(assets/nebraska.jpeg)");
             break;
         case "nv" || "nevada":
-            $('body').css('background', "url(assets/nevada.png)");
+            $('body').css('background', "url(assets/nevada.jpeg)");
             break;
         case "nh" || "new hampshire":
-            $('body').css('background', "url(assets/new_hampshire.png)");
+            $('body').css('background', "url(assets/new_hampshire.jpeg)");
             break;
         case "nj" || "new jersey":
-            $('body').css('background', "url(assets/new_jersey.png)");
+            $('body').css('background', "url(assets/new_jersey.jpeg)");
             break;
         case "nm" || "new mexico":
-            $('body').css('background', "url(assets/new_mexico.png)");
+            $('body').css('background', "url(assets/new_mexico.jpeg)");
             break;
         case "ny" || "new york":
-            $('body').css('background', "url(assets/new_york.png)");
+            $('body').css('background', "url(assets/new_york.jpeg)");
             break;
         case "nc" || "north carolina":
-            $('body').css('background', "url(assets/north_carolina.png)");
+            $('body').css('background', "url(assets/north_carolina.jpeg)");
             break;
         case "nd" || "north dakota":
-            $('body').css('background', "url(assets/north_dakota.png)");
+            $('body').css('background', "url(assets/north_dakota.jpeg)");
             break;
         case "oh" || "ohio":
-            $('body').css('background', "url(assets/ohio.png)");
+            $('body').css('background', "url(assets/ohio.jpeg)");
             break;
         case "ok" || "oklahoma":
-            $('body').css('background', "url(assets/oklahoma.png)");
+            $('body').css('background', "url(assets/oklahoma.jpeg)");
             break;
         case "or" || "oregon":
-            $('body').css('background', "url(assets/oregon.png)");
+            $('body').css('background', "url(assets/oregon.jpeg)");
             break;
         case "pa" || "pennsylvania":
-            $('body').css('background', "url(assets/pennsylvania.png)");
+            $('body').css('background', "url(assets/pennsylvania.jpeg)");
             break;
         case "ri" || "rhode island":
-            $('body').css('background', "url(assets/rhode_island.png)");
+            $('body').css('background', "url(assets/rhode_island.jpeg)");
             break;
         case "sc" || "south carolina":
-            $('body').css('background', "url(assets/south_carolina.png)");
+            $('body').css('background', "url(assets/south_carolina.jpeg)");
             break;
         case "sd" || "south dakota":
-            $('body').css('background', "url(assets/south_dakota.png)");
+            $('body').css('background', "url(assets/south_dakota.jpeg)");
             break;
         case "tn" || "tennessee":
-            $('body').css('background', "url(assets/tennessee.png)");
+            $('body').css('background', "url(assets/tennessee.jpeg)");
             break;
         case "tx" || "texas":
-            $('body').css('background', "url(assets/texas.png)");
+            $('body').css('background', "url(assets/texas.jpeg)");
             break;
         case "ut" || "utah":
-            $('body').css('background', "url(assets/utah.png)");
+            $('body').css('background', "url(assets/utah.jpeg)");
             break;
         case "vt" || "vermont":
-            $('body').css('background', "url(assets/vermont.png)");
+            $('body').css('background', "url(assets/vermont.jpeg)");
             break;
         case "va" || "virginia":
-            $('body').css('background', "url(assets/virginia.png)");
+            $('body').css('background', "url(assets/virginia.jpeg)");
             break;
         case "wa" || "washington":
-            $('body').css('background', "url(assets/washington.png)");
+            $('body').css('background', "url(assets/washington.jpeg)");
             break;
         case "wv" || "west virginia":
-            $('body').css('background', "url(assets/west_virginia.png)");
+            $('body').css('background', "url(assets/west_virginia.jpeg)");
             break;
         case "wi" || "wisconsin":
-            $('body').css('background', "url(assets/wisconsin.png)");
+            $('body').css('background', "url(assets/wisconsin.jpeg)");
             break;
         case "wy" || "wyoming":
-            $('body').css('background', "url(assets/wyoming.png)");
+            $('body').css('background', "url(assets/wyoming.jpeg)");
             break;
         default:
-            $('body').css('background', "url(assets/default.png)");
+            $('body').css('background', "url(assets/default.jpeg)");
     }
 }
